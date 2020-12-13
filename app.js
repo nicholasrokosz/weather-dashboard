8c4689f10267cb36b61d7b2301a792e8a12b1bf8
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2020-12-12 17:14:24 -0700

    add comments

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-// $('#search-btn').click(function () {
-//   const city = $('#search').val();
-//   const myKey = 'a8264870351b77d127763c42dc124460';
-//   queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myKey}`;
-
-//   $.get(queryURL).then(function (data) {
-//     $('.today').empty();
-//     $('<h1>').text(data.name).appendTo($('.today'));
-//     $('<p>')
-//       .text(`Temp: ${Math.floor(1.8 * (data.main.temp - 273) + 32)}°F`)
-//       .appendTo($('.today'));
-//   });
-// });
-
 $(document).ready(() => {
   const today = luxon.DateTime.local().toFormat('D');
   const myKey = 'a8264870351b77d127763c42dc124460';
@@ -38,8 +24,7 @@ $(document).ready(() => {
     window.localStorage.setItem('history', JSON.stringify(history));
     $('#search').val('');
   });
-
-  //   Generate weather's data function
+  // main function
   function getWeather(city) {
     const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myKey}`;
     $.ajax({
@@ -67,45 +52,31 @@ $(document).ready(() => {
       const humidity = $('<p>').text('Humidity: ' + data.main.humidity + ' %');
       const wind = $('<p>').text('Wind Speed: ' + data.wind.speed + ' MPH');
 
-      // Append element to the page
       $('.city-info').append(city, temp, humidity, wind);
       makeHistory();
-
-      // SECOND CALL
-
-      // Function to build URL for second request
 
       function buildSecondURL() {
         let querySecondURL = 'https://api.openweathermap.org/data/2.5/onecall?';
 
-        //   Create object to keep keys for URL
         let querySecondParams = { appid: 'a8264870351b77d127763c42dc124460' };
         querySecondParams.lat = data.coord.lat;
         querySecondParams.lon = data.coord.lon;
 
-        //   Return URL
         return querySecondURL + $.param(querySecondParams);
       }
-      // Variable to fire previous function
+
       let secondQueryURL = buildSecondURL();
 
-      // Second request
       $.ajax({
         url: secondQueryURL,
         method: 'GET',
       }).then(function (response) {
-        // Variable to keep request for specific data
         let UVI = response.current.uvi;
-
-        //   Create element for UV index
         let indexUV = $('<p>')
           .text('UV Index: ')
           .append($('<span>').attr('class', 'uviColor').text(UVI));
 
-        // Append created element to the text area
         $('.city-info').append(indexUV);
-
-        //   Conditions for displaing color depending on data from server
         if (UVI <= 3) {
           $('.uviColor').css('color', 'green');
         } else if (UVI > 3 && UVI < 6) {
@@ -118,18 +89,13 @@ $(document).ready(() => {
           $('.uviColor').css('color', 'violet');
         }
 
-        //   Loop for display 5-day forecast
+        // display 5-day forecast
         for (let i = 1; i < 6; i++) {
-          // Variables for the loop
           let F1 = Math.floor((response.daily[i].temp.day - 273.15) * 1.8 + 32);
           let dateInUNIX = response.daily[i].dt;
           let daysInRow = luxon.DateTime.fromSeconds(dateInUNIX).toFormat('D');
-
-          // Create element for the date
           $('#day' + [i]).append($('<h6>').text(daysInRow));
           let condition = response.daily[i].weather[0].main;
-
-          // Conditions for displaing icon depending on data from server
           if (condition == 'Clear') {
             $('#day' + [i]).append($('<i>').attr('class', 'fas fa-sun'));
           } else if (condition == 'Rain') {
@@ -139,15 +105,11 @@ $(document).ready(() => {
           } else if (condition == 'Clouds') {
             $('#day' + [i]).append($('<i>').attr('class', 'fas fa-cloud'));
           }
-
-          // Create element for the temperature
           $('#day' + [i]).append(
             $('<p>')
               .attr('class', 'pForForecast')
               .text('Temp: ' + F1 + 'F')
           );
-
-          // Create element for the humidity
           $('#day' + [i]).append(
             $('<p>')
               .attr('class', 'pForForecast')
@@ -158,7 +120,7 @@ $(document).ready(() => {
     });
   }
 
-  // Function to append city names to the history box
+  // create previously searched cities set
   function makeHistory() {
     function makeRow(text) {
       $('#prev-cities').prepend(
@@ -171,7 +133,7 @@ $(document).ready(() => {
     }
   }
 
-  // Function to clear history box and local storage
+  // function to clear history box and local storage
   $('#clearBtn').on('click', function () {
     localStorage.clear();
     $('#prev-cities').empty();
@@ -181,8 +143,6 @@ $(document).ready(() => {
   if (history.length > 0) {
     getWeather(history[history.length - 1]);
   }
-
-  // Function to display previous data if you click city name in history box
   function historyOnClick(event) {
     const histEl = event.target;
     if (event.target.matches('h5')) {
@@ -198,7 +158,5 @@ $(document).ready(() => {
       getWeather(pastCity);
     }
   }
-
-  // Event listener for previous funstion
-  $(document).on('click', historyOnClick);
+  $(document).click(historyOnClick);
 });
